0f5367d814d7a5e30e7169a8ca86aa03050fd4fc
Adarsha Ghimire <adarshghimire2005@gmail.com>
2025-11-22 19:17:10 +0545

    basic config

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
     return (
-        <main>
-            <h1 className="text-center text-3xl mt-10">Hello, World!</h1>
-            <h1 className="text-center text-3xl mt-5">Welcome To Cinematrix</h1>
+        <main className="flex flex-col items-center gap-10">
+            <h1 className="mt-5 text-center text-3xl">Welcome To Cinematrix!</h1>
+            <p className="w-100 text-center text-lg">
+                Cinematrix is a web based movie catalogue where users can browse, maintain watchlists and rate movies
+                and the app provides personalized movie recommendations to the user based on their ratings and
+                activities.
+            </p>
         </main>
     );
 }
